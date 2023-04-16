a9cd244a0708a915016673018cb76536910310a6
Angel Leon <angel.leon.ram@gmail.com>
2023-04-16 17:09:04 -0300

    Finalización de las funciones, ahora la página es capaz de devolver los datos de: Cantidad de alumnos ingresados, y Promedio final entre todos los alumnos

--- js/main.js
+++ js/main.js
@@ -1,34 +1,38 @@
-// let alumno = prompt ("Ingrese el nombre del alumno: (o escriba `ESC` o `0` para salir en cualquier momento)")
-// while ((alumno != "ESC") && (alumno != "0")){
-//     alert ("Alumno: " + alumno);
+//Promedios ingresados
+let promedios = [];
 
+//Ciclo 
+let permiso = prompt ("Si desea promediar a un alumno escriba `Si`, de lo contrario, escriba cualquier letra)")
+while (permiso === "Si"){
+    alert ("Gracias por usar nuestra plataforma de promediación");
+    studentProm()
+    permiso = prompt("Si desea continuar promediando escriba `Si`, de lo contrario, escriba cualquier letra")
+}
 
-
-
-
-//     alumno = prompt ("Ingrese el nombre del siguiente alumno (o escriba ESC `0` para salir)");
-
-// }
-
-// let promedios = prompt ("A continuación mostraremos el promedio de cada estudiante: (o escriba ESC o `0` para salir en cualquier momento)" /n, alumno + promedio )
-// while ((promedios != "ESC") && (promedios != "0")){
-//     alert ("");
-//     entrada = prompt ("El promedio de (o escriba ESC `0` para salir)");
-// }
-studentProm()
+//Función principal
 function studentProm (){
     let alumno = prompt ("Ingrese el nombre del alumno");
     alert ("El nombre del alumno es: " + alumno);
     promedio();
-
-    function promedio (){
-        let nota1 = Number(prompt ("Ingrese la nota del primer exámen: "));
-        let nota2 = Number(prompt ("Ingrese la nota del segundo exámen: "));
-        let nota3 = Number(prompt ("Ingrese la nota del tercer exámen: "));
-    
-        let sumatoria = nota1 + nota2 + nota3;
-        let promedioTotal = sumatoria / 3;
-        alert ("El promedio del alumno es: " + promedioTotal);
-    }
+}
+    //Función de promediación
+function promedio (){
+    let nota1 = Number(prompt ("Ingrese la nota del primer exámen: "));
+    let nota2 = Number(prompt ("Ingrese la nota del segundo exámen: "));
+    let nota3 = Number(prompt ("Ingrese la nota del tercer exámen: "));
+    let sumatoria = nota1 + nota2 + nota3;
+    let promedioTotal = sumatoria / 3;
+    promedios.push(promedioTotal);
+    alert ("El promedio del alumno es: " + promedioTotal);
 }
 
+// Promedio de todos los valores almacenados en la variable "promedios"
+let sumaPromedios = 0;
+for (let i = 0; i < promedios.length; i++) {
+    sumaPromedios += promedios[i];
+}
+let promedioFinal = sumaPromedios / promedios.length;
+
+//Devolución final de datos
+alert ("La cantidad de alumnos ingresados es: " + promedios.length)
+alert("El promedio final de todos sus alumnos es: " + promedioFinal);
