c0c05b09b099da4aedebf46c7a831d4821c7ad8b
Angel Leon <angel.leon.ram@gmail.com>
2023-05-01 15:02:44 -0300

    Optimización final del código, funcionalidad casi total

--- js/main.js
+++ js/main.js
@@ -8,11 +8,19 @@ while (permiso.toLowerCase() == "si"){
 };
 
 //Devolución final de datos
-const OPCION_ALUMNOS_INGRESADOS = 1;
-const OPCION_RESUMEN_ALUMNOS = 2;
-const OPCION_SALIR = 3;
+const OPCION_ALUMNOS = 1   
+const OPCION_ALUMNOS_PROMEDIOS = 2;
+const OPCION_ALUMNOS_INGRESADOS = 3;
+const OPCION_RESUMEN_ALUMNOS = 4;
+const OPCION_SALIR = 5;
 
 switch (mostrarMenu()) {
+    case OPCION_ALUMNOS: 
+        namesDev();
+        break;
+    case OPCION_ALUMNOS_PROMEDIOS:
+        namesProm();
+        break;
     case OPCION_ALUMNOS_INGRESADOS:
       showData();
       break;
@@ -25,14 +33,14 @@ switch (mostrarMenu()) {
     default:
       alert("Opción inválida");
       break;
-  }
+};
 
 //FUNCIONES
 //CORE FUNCTION
 function studentProm() {
     alert ("Gracias por usar nuestra plataforma de promediación");
     dataSolicitation();
-}
+};
 //Solicitud de datos
 function dataSolicitation(){
     let nombreAlumno = nameInput();
@@ -41,8 +49,8 @@ function dataSolicitation(){
     let nota_3 = pedirNota(3);
     let promedio = calcularPromedio(nota_1, nota_2, nota_3);
     studentAdd (nombreAlumno, nota_1, nota_2, nota_3, promedio);
-    alert("El promedio del alumno es: " + promedio)
-}
+    alert(`El promedio del alumno ${nombreAlumno} es: ${promedio}`);
+};
 
 
 //Pedir el nombre
@@ -58,42 +66,56 @@ function nameInput(){
       }
     }
     return alumno;
-}
+};
 //Solicitud de nota y validación
 function pedirNota(notaNum){
     let nota = Number(prompt(`Ingrese la nota del ${notaNum}º examen:`));
     while (isNaN(nota) || nota < 0 || nota > 20) {
-      alert("Error: La nota debe estar entre 0 y 20");
-      nota = Number(prompt(`Ingrese nuevamente la nota del ${notaNum}º examen:`));
+        alert("Error: La nota debe estar entre 0 y 20");
+        nota = Number(prompt(`Ingrese nuevamente la nota del ${notaNum}º examen:`));
     }
     return nota;
-}
+};
 //Cálculo del promedio
 function calcularPromedio(nota_1, nota_2, nota_3){
     let promedio = (nota_1 + nota_2 + nota_3) / 3;
     return promedio.toFixed(2);
-}
+};
 //Adición de datos al arreglo
 function studentAdd(nombre, nota_1, nota_2, nota_3, promedio){
     let estudiante = {nombre: nombre, nota1: nota_1, nota2: nota_2, nota3: nota_3, promedio: promedio};
     estudiantes.push(estudiante);
-}
+};
 
 
 //FUNCIONES DEL SWITCH
 //CICLO PRINCIPAL
 function mostrarMenu() {
     let opcion = 0;
-    while (opcion < OPCION_ALUMNOS_INGRESADOS || opcion > OPCION_SALIR) {
-      opcion = Number(prompt(`Ingrese una opción:
+    while (opcion < OPCION_ALUMNOS || opcion > OPCION_SALIR){
+        opcion = Number(prompt(`Ingrese una opción:
+        ${OPCION_ALUMNOS}. Mostrar nombres de los alumnos ingresados
+        ${OPCION_ALUMNOS_PROMEDIOS}. Mostrar nombres de los alumnos y sus promedios
         ${OPCION_ALUMNOS_INGRESADOS}. Datos de todos los alumnos ingresados
         ${OPCION_RESUMEN_ALUMNOS}. Número total de estudiantes que ingresó y el promedio total entre ellos
         ${OPCION_SALIR}. Salir`));
-    }
+    };
     return opcion;
   }
 
 //FUNCIONES DE DEVOLUCIÓN
+//Devolución de nombres
+function namesDev(){
+    let mensaje = estudiantes.map(estudiante => estudiante.nombre).join("\n");
+    alert(mensaje);
+};
+
+//Devolución de nombres y promedios
+function namesProm(){
+    let mensaje = estudiantes.map(estudiante => estudiante.nombre + " ---- " + estudiante.promedio).join("\n");
+    alert(mensaje);
+};
+
 //Devolucion de cada estudiante
 function showData(){
     let mensaje = "";
@@ -103,20 +125,20 @@ function showData(){
                    "Nota 1: " + estudiantes[i].nota1 + "\n" +
                    "Nota 2: " + estudiantes[i].nota2 + "\n" +
                    "Nota 3: " + estudiantes[i].nota3 + "\n" +
-                   "Promedio: " + estudiantes[i].promedios + "\n\n";
+                   "Promedio: " + estudiantes[i].promedio + "\n\n";
     }
     alert(mensaje);
 };
 //Devolución resumida
 function showData_2(){
-    let promedioFinal = 0;
+    let sumaPromedios = 0;
     let mensaje = "";
     for (let i = 0; i < estudiantes.length; i++) {
-        promedioFinal += estudiantes[i].promedios;
+        sumaPromedios += estudiantes[i].promedios;
     }
-    promedioFinal = promedioFinal/estudiantes.length;
+    promedioAbsoluto = sumaPromedios / estudiantes.length;
     mensaje += "Cantidad total de estudiantes ingresados: " + estudiantes.length + "\n" +
-                "Promedio final de todos los estudiantes: " + promedioFinal;
+                "Promedio final de todos los estudiantes: " + promedioAbsoluto;
     alert(mensaje);
 };
 
